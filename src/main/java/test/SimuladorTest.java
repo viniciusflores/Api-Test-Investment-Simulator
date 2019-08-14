adffd80572ae7610f2f1f2340227d60f60c26377
vinicius flores <viniciusflores379@gmail.com>
2019-08-14 00:46:10 -0300

    :sparkles: improvement SimuladorTest

--- src/main/java/test/SimuladorTest.java
+++ src/main/java/test/SimuladorTest.java
@@ -2,6 +2,7 @@ package test;
 
 import static io.restassured.RestAssured.given;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import core.BaseTest;
@@ -9,10 +10,15 @@ import core.BaseTest;
 public class SimuladorTest extends BaseTest {
 	@Test
 	public void checkStatusTrue() {
-		given()
-		.when()
-			.get("/simulador")
-		.then()
-			.statusCode(200);
+		given().when().get("/simulador").then().statusCode(200);
 	}
+
+	@Test
+	public void checkSizeMonthAndValue() {
+		// ArrayList<String>
+		int months = given().when().get("/simulador").then().statusCode(200).extract().path("meses.findAll().size()");
+		int values = given().when().get("/simulador").then().statusCode(200).extract().path("meses.findAll().size()");
+		Assert.assertEquals(months, values);
+	}
+	
 }
